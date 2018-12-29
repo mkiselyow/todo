44a41a2c236be053b0cf559abef503a85445d097
Maxim Kiselyow <m.kiselyow@gmail.com>
2018-12-29 14:21:08 +0200

    *onClickAdd part3

--- src/components/InputNewItem/InputNewItem.js
+++ src/components/InputNewItem/InputNewItem.js
@@ -6,16 +6,42 @@ export default class InputNewItem extends Component {
   constructor() {
     super();
     this.state = {
-      searchInput: ""
+      textInput: ""
     };
-  }
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.props.onClickAdd(this.state.textInput);
+      this.clearInput();
+    } else {
+      this.onTextInput(e.key);
+    }
+  };
+
+  onTextInput = (newSymbol) => {
+    this.setState(({textInput}) => {
+      return {
+        textInput: (textInput + newSymbol)
+      }
+    })
+  };
+
+  clearInput = () => {
+    this.setState(({textInput}) => {
+      return {
+        textInput: ""
+      }
+    })
+  };
 
   render() {
     return (
       <input type="text"
+             value={this.state.textInput}
              className="form-control mt-3"
              placeholder="Enter new todo name"
-             onChange={() => this.props.onClickAdd("Ssssssssss")}
+             onKeyPress={this.handleKeyPress}
       />
     )
   }
