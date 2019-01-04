17ad0f1ab4ee7c817ad77702f5ce2034544d9871
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-01-04 11:58:26 +0200

    *ItemStatusFilter to controlled component

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -3,21 +3,40 @@ import React, { Component } from 'react';
 import './ItemStatusFilter.css';
 
 export default class ItemStatusFilter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeBtn: ''
+    };
+  };
+
+  handleBtnPress = (btnName) => {
+    this.setState({activeBtn: btnName})
+  };
+
+  buttons = [
+    { name: '', label: 'All' },
+    { name: false, label: 'Active' },
+    { name: true, label: 'Done' }
+  ];
+
   render() {
+    const buttons = this.buttons.map(({name, label}) => {
+      const className = (this.state.activeBtn === name)
+        ? 'btn btn-info'
+        : 'btn btn-outline-secondary';
+
+      return (
+        <button
+          type="button"
+          onClick={() => {this.handleBtnPress(name); this.props.updateStateProperty(name)}}
+          className={className}>{label}</button>
+        );
+    });
+
     return (
       <div>
-        <button
-          type="button"
-          onClick={() => this.props.updateStateProperty('')}
-          className="btn btn-info">All</button>
-        <button
-          type="button"
-          onClick={() => this.props.updateStateProperty(false)}
-          className="btn btn-outline-secondary">Active</button>
-        <button
-          type="button"
-          onClick={() => this.props.updateStateProperty(true)}
-          className="btn btn-outline-secondary">Done</button>
+        { buttons }
       </div>
     );
   }
